d89e6ed28c7256700439177199e9d109f24a00ca
KIMENYI Kevin <kimenyikevin@gmail.com>
2019-08-05 20:43:09 +0100

    feature(homepage):introduction of website
    edit of main page and footer
    
    [continues: #167675587]

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+// // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function() {
   scrollFunction()};
 function scrollFunction() {
@@ -18,4 +18,19 @@ function toggleMenu() {
   } else{
       menuBox.style.display = "block";
   }
+}
+
+var myIndex = 0;
+carousel();
+function carousel() {
+  var i;
+  // var x = document.querySelectorAll('.mySlides');
+  var x = document.getElementsByClassName("switch");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 5000); // Change image every 2 seconds
 }
